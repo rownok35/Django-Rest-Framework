66e558dac9586d0ff18806097cff4182a2f3fc16
rownok35 <rownokrezwan@gmail.com>
2021-08-13 19:08:58 +0600

    test case added

--- user_app/tests.py
+++ user_app/tests.py
@@ -1,3 +1,40 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your tests here.
+from rest_framework import status
+from rest_framework.test import APITestCase
+from rest_framework.authtoken.models import Token
+
+
+class RegisterTestCase(APITestCase):
+
+    def test_register(self):
+        data = {
+            "username": "testcase",
+            "email": "sam1@gmail.com",
+            "password":  "1234asdf",
+            "password2": "1234asdf"
+        }
+        response = self.client.post(reverse('register'), data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
+class LoginLogoutTestCase(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username="sam1",
+                                             password="1234asdf")
+
+    def test_login(self):
+        data = {
+            "username": "sam1",
+            "password": "1234asdf"
+        }
+        response = self.client.post(reverse('login'), data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_logout(self):
+        self.token = Token.objects.get(user__username="sam1")
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        response = self.client.post(reverse('logout'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
